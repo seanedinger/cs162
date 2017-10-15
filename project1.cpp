7f6b6152b6ee7533339b763f41e0fc795aa9d878
Sean Edinger <sedinger007@gmail.com>
2017-10-14 18:52:20 -0700

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_CHAR = 101;
+
+int main()
+{
+	float   itemCost;
+	float   totalCost;
+	char    response;
+
+	cout << endl << "Welcome to your Ez Shopping Cart!" << endl;
+
+	char    itemName[MAX_CHAR];
+
+	//get item from user
+	cout << "Please enter the item name: ";
+	cin.get(itemName, MAX_CHAR, '\n');
+	while (!cin)
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		cout << endl << "Invalid input! Please try again: ";
+		cin >> itemName;
+	}
+	cin.ignore(100, '\n');
+
+	//get itemCost of item from user
+	cout << "Please enter the cost of the item: (e.g. 12.34) ";
+	cin >> itemCost;
+	while (!cin) {
+		cin.clear();
+		cin.ignore(100, '\n');
+		cout << endl << "You've entered an illegal item cost. Please try again: "
+		cin >> item;
+	}
+
+	//allow user to enter more items
+	do
+	{
+		//calculate total cost and display it
+		totalCost += itemCost;
+
+		cout.setf(ios::fixed, ios::floatfield);
+		cout.setf(ios::showpoint);
+		cout.precision(2);
+		cout << endl << "Total Cost: " << totalCost << endl;
+
+		//update loop control variable "response"
+		cout << endl << "Want to add another item? (Y/N) ";
+		cin >> response;
+		cin.ignore(100, '\n');
+		response = tolower(response);
+	} while (response == 'y');
+
+	cout << endl << "Thank you for using Ez Shopping Cart!" << endl;
+
+	return 0;
+}
