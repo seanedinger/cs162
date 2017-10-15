aec2632607fee4808e2d5448e4531a955c673555
Sean Edinger <sedinger007@gmail.com>
2017-10-15 15:15:35 -0700

    Add files via upload

--- project1.cpp
+++ project1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -32,11 +33,46 @@ int main()
 	while (!cin) {
 		cin.clear();
 		cin.ignore(100, '\n');
-		cout << endl << "You've entered an illegal item cost. Please try again: "
-		cin >> item;
+		cout << endl << "You've entered an illegal item cost. Please try again: " << endl;
+		cin >> itemCost;
 	}
 
+	
 	//allow user to enter more items
+	do
+	{
+		//get item from user
+		cout << "Please enter the item name: ";
+		cin.get(itemName, MAX_CHAR, '\n');
+		while (!cin)
+		{
+			cin.clear();
+			cin.ignore(100, '\n');
+			cout << endl << "Invalid input! Please try again: ";
+			cin >> itemName;
+		}
+		cin.ignore(100, '\n');
+
+		//get itemCost of item from user
+		cout << "Please enter the cost of the item: (e.g. 12.34) ";
+		cin >> itemCost;
+		while (!cin) {
+			cin.clear();
+			cin.ignore(100, '\n');
+			cout << endl << "You've entered an illegal item cost. Please try again: " << endl;
+			cin >> itemCost;
+		}
+		//calculate total cost and display it
+		totalCost += itemCost;
+
+		//update loop control variable "response"
+		cout << endl << "Want to add another item? (Y/N) ";
+		cin >> response;
+		cin.ignore(100, '\n');
+		response = tolower(response);
+	} while (response == 'y');
+	
+	//if user is done adding items to cart - calculate grand total:
 	do
 	{
 		//calculate total cost and display it
@@ -48,7 +84,7 @@ int main()
 		cout << endl << "Total Cost: " << totalCost << endl;
 
 		//update loop control variable "response"
-		cout << endl << "Want to add another item? (Y/N) ";
+		cout << endl << "Ready to calculate grand total? (Y/N) ";
 		cin >> response;
 		cin.ignore(100, '\n');
 		response = tolower(response);
